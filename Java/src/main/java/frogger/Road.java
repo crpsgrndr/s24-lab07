fe627e43d9315f8529109af08ed1f82920bb6211
Usukhbayar Orosoo <euaxlura@yahoo.com>
2025-03-26 15:53:35 +0800

    Refactored Anti-Patterns

--- Java/src/main/java/frogger/Road.java
+++ Java/src/main/java/frogger/Road.java
@@ -1,10 +1,5 @@
 package frogger;
 
-/**
- * Refactor Task 1.
- *
- * @author Zishen Wen (F22), Deyuan Chen (S22)
- */
 public class Road {
     private final boolean[] occupied;
 
@@ -14,5 +9,13 @@ public class Road {
 
     public boolean[] getOccupied() {
         return this.occupied;
+    }
+
+    public boolean isOccupied(int position) {
+        return occupied[position];
+    }
+
+    public boolean isValidPosition(int position) {
+        return position >= 0 && position < occupied.length;
     }
 }
